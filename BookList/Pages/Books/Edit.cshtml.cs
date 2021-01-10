6b637031758971765cdee6ed66fc980fe8da9581
Rizvi-07 <tariqul.rizvi@gmail.com>
2021-01-11 03:29:34 +0600

    Edit and delete implementation

---
+++ BookList/Pages/Books/Edit.cshtml.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookList.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookList.Pages.Books
+{
+    public class EditModel : PageModel
+    {
+        private ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public async Task OnGet(int id)
+        {
+            Book = await _db.Book.FindAsync(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if(ModelState.IsValid)
+            {
+                var bookFromDb = await _db.Book.FindAsync(Book.Id);
+                bookFromDb.Name = Book.Name;
+                bookFromDb.Author = Book.Author;
+                bookFromDb.ISBN = Book.ISBN;
+
+                await _db.SaveChangesAsync(); //change in database
+
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return RedirectToPage();
+            }
+        }
+    }
+}
--- BookList/Pages/Books/Index.cshtml.cs
+++ BookList/Pages/Books/Index.cshtml.cs
@@ -12,15 +12,30 @@ namespace BookList.Pages.Books
     public class IndexModel : PageModel
     {
         private readonly ApplicationDbContext _db;
+
         public IndexModel(ApplicationDbContext db)
         {
             _db = db;
         }
 
-        public IEnumerable<Book> Books { get; set; }
+        public IEnumerable<Book> Books { get; set; } 
+
         public async Task OnGet()
         {
             Books = await _db.Book.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var book = await _db.Book.FindAsync(id);
+            if(book == null)
+            {
+                return NotFound();
+            }
+            _db.Book.Remove(book);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
         }
     }
 }
